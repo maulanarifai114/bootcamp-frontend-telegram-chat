06ca384632602fdc607426be48d898725f514b46
maulanarifai114 <maulanarifai114@gmail.com>
2020-12-29 19:41:10 +0700

    add geo v3 (use this)

--- src/views/geolocation/Geo.vue
+++ src/views/geolocation/Geo.vue
@@ -1,49 +1,25 @@
 <template>
-
-  <div style="height: 350px;">
-    <div class="info" style="height: 15%">
-      <span>Center: {{ center }}</span>
-      <span>Zoom: {{ zoom }}</span>
-      <span>Bounds: {{ bounds }}</span>
-    </div>
-    <l-map
-      style="height: 80%; width: 100%"
-      :zoom="zoom"
-      :center="center"
-      @update:zoom="zoomUpdated"
-      @update:center="centerUpdated"
-      @update:bounds="boundsUpdated"
-    >
-      <l-tile-layer :url="url"></l-tile-layer>
-    </l-map>
-  </div>
+<l-map style="height: 350px" :zoom="zoom" :center="center">
+<l-tile-layer :url="url"></l-tile-layer>
+<l-marker :lat-lng="markerLatLng" ></l-marker>
+</l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer } from 'vue2-leaflet'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
 
 export default {
   components: {
     LMap,
-    LTileLayer
+    LTileLayer,
+    LMarker
   },
   data () {
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoom: 3,
-      center: [47.413220, -1.219482],
-      bounds: null
-    }
-  },
-  methods: {
-    zoomUpdated (zoom) {
-      this.zoom = zoom
-    },
-    centerUpdated (center) {
-      this.center = center
-    },
-    boundsUpdated (bounds) {
-      this.bounds = bounds
+      center: [47.313220, -1.319482],
+      markerLatLng: [47.313220, -1.319482]
     }
   }
 }
